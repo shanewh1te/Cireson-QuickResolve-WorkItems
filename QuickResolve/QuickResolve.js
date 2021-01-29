3872bf88ade7d56426eb0d50bed0ffdb4f879b57
shanewh1te <53583731+shanewh1te@users.noreply.github.com>
2021-01-29 15:34:46 +0000

    Update QuickResolve.js

--- QuickResolve/QuickResolve.js
+++ QuickResolve/QuickResolve.js
@@ -3,9 +3,9 @@ app.custom.formTasks.add('Incident', null, function(formObj, viewModel) {
         if (window.location.href.indexOf("/Incident/New/") !== -1 && viewModel.Status.Id === "2b8830b6-59f0-f574-9c2a-f4b4682f1681") {
             viewModel.set("ResolvedDate", new Date().toISOString());
             viewModel.set("RelatesToTroubleTicket", {
-				"BaseId": session.user.Id,
-				"Name": session.user.Name
-			});
+		"BaseId": session.user.Id,
+		"Name": session.user.Name
+	    });
         }
     });
 });
